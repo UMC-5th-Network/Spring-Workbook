556266a3be13e47da159ff0c3e6ecb0678465299
DDeo99 <yoondukwoo@gmail.com>
2023-11-12 01:14:40 +0900

    양방향 매핑

--- src/main/java/umc/study/domain/Store.java
+++ src/main/java/umc/study/domain/Store.java
@@ -1,31 +1,36 @@
 package umc.study.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import umc.study.domain.common.BaseEntity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity(name="Store")
 @Table(name="store")
 public class Store extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
 
     private String address;
 
-    private float score;
+    private Float score;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="region_id",referencedColumnName = "id")
     private Region region;
 
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
 
 }
