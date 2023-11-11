ee2d63a0b1eb46080cb6f16617b8f9d5fc9f930e
DDeo99 <yoondukwoo@gmail.com>
2023-11-12 00:44:11 +0900

    ReviewImage Entity

---
+++ src/main/java/umc/study/domain/ReviewImage.java
@@ -0,0 +1,23 @@
+package umc.study.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name="ReviewImage")
+@Table(name="review_image")
+public class ReviewImage {
+    @Id
+    private long id;
+
+    private String image_url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="review_id",referencedColumnName = "id")
+    private Review review;
+}
