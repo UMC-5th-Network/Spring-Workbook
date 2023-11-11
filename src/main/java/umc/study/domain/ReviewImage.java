c89b847351856469ad68dc35ca479a918070f767
DDeo99 <yoondukwoo@gmail.com>
2023-11-12 01:14:04 +0900

    ReviewImage Entity

--- src/main/java/umc/study/domain/ReviewImage.java
+++ src/main/java/umc/study/domain/ReviewImage.java
@@ -1,19 +1,20 @@
 package umc.study.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import umc.study.domain.common.BaseEntity;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity(name="ReviewImage")
 @Table(name="review_image")
 public class ReviewImage extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String image_url;
