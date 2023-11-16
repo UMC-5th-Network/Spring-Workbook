75f4ec14067c34b072ef49de8eebcaa9124fbff9
Kang Hyun Woo <gusdn741021@gmail.com>
2023-11-16 16:05:23 +0900

    feat : api 응답 통일 코드 작성

---
+++ homework/src/main/java/umc5th/homework/apiPayload/code/BaseCode.java
@@ -0,0 +1,6 @@
+package umc5th.homework.apiPayload.code;
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ homework/src/main/java/umc5th/homework/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,6 @@
+package umc5th.homework.apiPayload.code;
+
+public interface BaseErrorCode {
+    public ErrorReasonDTO getReason();
+    public ErrorReasonDTO getReasonHttpStatus();
+}
