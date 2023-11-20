abe481e9df2768a6505a288de1bf3ff206121376
Sj0-0i <sjy140801@dgu.ac.kr>
2023-11-20 15:45:28 +0900

    Chore: code check

--- src/main/java/umc/springUMC/web/dto/TempResponse.java
+++ src/main/java/umc/springUMC/web/dto/TempResponse.java
@@ -7,6 +7,12 @@ import lombok.NoArgsConstructor;
 
 public class TempResponse {
 
+    /**
+     * DTO - 큰 묶음으로 class 생성 ex) MemberDTO 등
+     *     - 내부적으로 static class 생성
+     *     - 범용적인 DTO 사용 가능해짐.
+     */
+
     @Builder
     @Getter
     @NoArgsConstructor
