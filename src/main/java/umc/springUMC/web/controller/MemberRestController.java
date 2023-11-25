02a8a9264443befbb0a75a9889db442e86de175c
Sj0-0i <sjy140801@dgu.ac.kr>
2023-11-26 03:12:02 +0900

    feat: create controller

---
+++ src/main/java/umc/springUMC/web/controller/MemberRestController.java
@@ -0,0 +1,25 @@
+package umc.springUMC.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.springUMC.apiPayload.ApiResponse;
+import umc.springUMC.web.dto.MemberRequestDTO;
+import umc.springUMC.web.dto.MemberResponseDTO;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request) {
+        return null;
+    }
+}
