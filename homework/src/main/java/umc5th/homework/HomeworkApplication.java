7e0919dd31d02e6ce2ed152a30467367bbb07921
Kang Hyun Woo <gusdn741021@gmail.com>
2023-11-27 23:12:01 +0900

    refactory: JpaAuditing 추가

--- homework/src/main/java/umc5th/homework/HomeworkApplication.java
+++ homework/src/main/java/umc5th/homework/HomeworkApplication.java
@@ -2,8 +2,10 @@ package umc5th.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
