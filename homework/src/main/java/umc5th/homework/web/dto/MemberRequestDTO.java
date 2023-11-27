25559a6c51c0c7b66205fd7d0ce7d14b53312877
Kang Hyun Woo <gusdn741021@gmail.com>
2023-11-27 22:51:45 +0900

    refactory: 맴버 DTO 검증 조건 추가

--- homework/src/main/java/umc5th/homework/web/dto/MemberRequestDTO.java
+++ homework/src/main/java/umc5th/homework/web/dto/MemberRequestDTO.java
@@ -1,5 +1,8 @@
 package umc5th.homework.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +16,19 @@ public class MemberRequestDTO {
 
     @Getter
     public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
         @ExistCategories
         List<Long> preferCategory;
