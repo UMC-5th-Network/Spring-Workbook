812f2e5ff64f676b530b468967a138d8fc63414b
Kang Hyun Woo <gusdn741021@gmail.com>
2023-12-03 22:50:16 +0900

    swagger를 이용한 api 명세 = contoller 메서드 정의만 해두기

--- homework/src/main/java/umc5th/homework/web/controller/StoreRestController.java
+++ homework/src/main/java/umc5th/homework/web/controller/StoreRestController.java
@@ -1,11 +1,14 @@
 package umc5th.homework.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc5th.homework.apiPayload.ApiResponse;
 import umc5th.homework.converter.StoreConverter;
 import umc5th.homework.domain.Store;
@@ -22,5 +25,25 @@ public class StoreRestController {
     public ApiResponse<StoreDTO.AddResponseDTO> add(@RequestBody @Valid StoreDTO.AddDTO request){
         Store store = storeService.addStore(request);
         return ApiResponse.onSuccess(StoreConverter.toAddDto(store));
+    }
+
+    /*
+    * Operation은 api에 대한 설명을 넣게 되며 summary, description으로 설명을 적는다
+    * ApiResponse로 이 api의 응답을 담게 되며 내부적으로 @ApiReponse로 각각의 응답들을 담게 된다.
+    * 에러 상황에 대해서만 content = 를 통해 형태를 알려주고(에러는 코드, 메시자만 중요하지 result는 필요ㅕ 없음)
+    * */
+    @GetMapping("/{storeId}/reviews")
+    @Operation(summary = "특정 가게의 리뷰 목록 조회 API",description = "특정 가게의 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!")
+    })
+    public ApiResponse<StoreDTO.ReviewPreViewListDTO> getReviewList(@PathVariable(name = "storeId") Long storeId){
+        return null;
     }
 }
